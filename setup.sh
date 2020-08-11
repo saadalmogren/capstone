3fc0444692a8f86a1d17a694f4a9f5ff44c3442c
saad <saad-mogren@hotmail.com>
2020-08-11 20:21:58 +0300

    add auth0 configuration

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-DATABASE_URL=DATABASE_URL=postgres://postgres:admin@localhost:5432/capstone_test
+DATABASE_URL=postgres://postgres:admin@localhost:5432/capstone_test #database url
+clientId=fIqAzFVMCGHU5zgV3OJBHVowsvY4QQXM #auth0 client id
+url=dev-w0twvam7.eu.auth0.com  #auth0 domain
+callbackURL=https://capstone-saad.herokuapp.com/actors
+audience=capstone #api audiance
